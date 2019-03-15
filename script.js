83b6dfc063a7fe3db9191c578d617b50f74cf3cc
Mickael Spennato <mickael.spennato@gmail.com>
2019-03-15 15:03:33 +0100

    ajout de l'animation javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+function myMove() {
+	var elem = document.getElementById("myAnimation");
+	var pos = 0;
+	var id = setInterval(frame, 1);
+	function frame() {
+		if (pos == 2500) {
+			clearInterval(id);
+		} else {
+			pos++;
+			elem.style.right = pos + 'px';
+		}
+	}
+}
